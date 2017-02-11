bcaa1ba509308d92efaa778e4e402641c85badc7
savinarp <savputramadhani@gmail.com>
2017-02-11 21:23:47 +0700

    Commit1

---
+++ src/atm/ATM.java
@@ -0,0 +1,18 @@
+package atm;
+
+
+/**
+ *
+ * @author Savina
+ */
+
+public class ATM {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+        
+    }
+    
+}
